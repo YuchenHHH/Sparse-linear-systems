5ee7854000cf322c2320597a96015508fa79d37c
YuchenH <244188948@qq.com>
2022-09-20 23:10:28 +0800

    delete gtest

--- cmake-build-debug/lib/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/course/Sparse lineat systems/Sparse-linear-systems/lib/googlemock
-# Build directory: E:/course/Sparse lineat systems/Sparse-linear-systems/cmake-build-debug/lib/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- cmake-build-debug/lib/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/course/Sparse lineat systems/Sparse-linear-systems/lib/googletest
-# Build directory: E:/course/Sparse lineat systems/Sparse-linear-systems/cmake-build-debug/lib/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
